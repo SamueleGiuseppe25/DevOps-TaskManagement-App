a74c5bd53b3e8425066723397f3a54bc0ebe38bd
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-02-11 10:42:23 +0000

    Added Styling

--- front-end/js/site.js
+++ front-end/js/site.js
@@ -3,8 +3,44 @@ function init() {
     FetchStudents()
 }
 
-async function FetchStudents() { 
-    const response = await fetch('https://localhost:7276/api/Student');
-    console.log('FetchStudents');
+async function FetchStudents() {
+    console.log('FetchStudents')
+    try {
+        const response = await fetch('https://localhost:7276/api/Student');
+       
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const tableBody = document.querySelector('#studentTable tbody');
+
+        tableBody.innerHTML = '';
+
+        const students = await response.json();
+
+        students.forEach(student => { 
+
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                <td>${student.id}</td>
+                <td>${student.name}</td>
+                <td>${student.email}</td>
+                <td>${student.age}</td>
+                <td>${student.course}</td>`
+                tableBody.appendChild(row);
+
+        });
+
+
+        console.log({students});
+
+    } catch (error) {
+
+        console.error(`HTTP error! status: ${response.status}`);
+
+     }
+
+
+
 }
 
