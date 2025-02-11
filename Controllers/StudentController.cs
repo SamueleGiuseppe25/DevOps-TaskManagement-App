5b7420db131790f463e91b5b2422c31744b5198b
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-02-11 09:54:33 +0000

    Added HTML file

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using epita_2025_core_api_001_students.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace epita_2025_core_api_001_students.Controllers
@@ -7,5 +8,25 @@ namespace epita_2025_core_api_001_students.Controllers
     [ApiController]
     public class StudentController : ControllerBase
     {
+        public StudentController() { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(GetStudents());
+
+        }
+
+        private List<Student> GetStudents()
+        {
+            return new List<Student>()
+    {
+                new Student() { Id = 1, Name = "John", Email = "john2@test.com", Age = 20, Course = "Computer Science" },
+                new Student() { Id = 2, Name = "Jane", Email = "john2@test.com", Age = 21, Course = "Maths" }, 
+                new Student() { Id = 3, Name = "Doe", Email = "john2@test.com", Age = 22, Course = "History" }
+
+    };
+        }
     }
 }
